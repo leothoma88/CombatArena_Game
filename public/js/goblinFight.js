fee4fc1de370db7d9373e8caca51710546782f16
pftho <pf.thomas97@gmail.com>
2022-10-19 20:39:54 -0400

    fixing stuff

--- public/js/goblinFight.js
+++ public/js/goblinFight.js
@@ -2,9 +2,53 @@
 //import getMOnsterData
 //import getCharacterData
 //import goblin image
+// const getCharacter = require("./getCharacterData");
+// // const getMonster  = require("./getMonsterData");
 
-const { default: getCharacter } = require("./getCharacterData");
-const { default: getMonster } = require("./getMonsterData");
+// const char = getCharacter(0);
+// const goblin = getMonster(0);
+let something = [];
+const getMonster = async (id) =>  {
+    // let x;
+    return await fetch(`/api/monsters/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    // .then((response) => response.json())
+    // .then((data) => {x = data})
+    
+    // .catch(error => console.log(error));
+    // return x;
+}
+// export default getMonster;
+// const goblin = getMonster(0);
 
-getCharacter(0);
-getMonster(1);
+// console.log('show goblin: ', goblin);
+// console.log(getMonster(0));
+// let goblin = getMonster(0);
+getMonster(0).then((res) => res.json()).then((data) => {
+    
+
+something.push(data);
+console.log(something)
+    console.log(data.strength)});
+
+console.log(something)
+
+const settingInterval = setInterval(() => {
+    if(something.length !== 0){
+    clearInterval(settingInterval);
+    } else {
+    console.log(something);
+    }
+}, 500)
+
+console.log(something[0])
+// let goblin = {
+//     id: 0,
+//     name: 'goblin',
+//     strength: 2,
+//     health: 2,
+// }
